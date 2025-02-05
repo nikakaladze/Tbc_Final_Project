96b9e1357204917cbe4e176daaeea50a07bb851b
nikakaladze <nikakaladze89@gmail.com>
2025-02-05 20:21:26 +0400

    chore:add some css code

---
+++ my-app/lib/constants.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'tbcFinalProject'
+export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || 'enjoy you life'
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION  || 'E-commerce website for Tbc'
